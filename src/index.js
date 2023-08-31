8ae9cea15342a308b7f2605e42ec49c7c668dc8b
hrithik <hrithikm485@gmail.com>
2023-09-01 00:35:04 +0530

    created a variable for JSX codes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-ReactDOM.render(
+const details=(
   <div>
   <h1 className="header"> This JSX!</h1>
   <p>It stands for JavaScript XML</p>
-  </div> ,
-document.getElementById("root")
-) 
+  </div>
+)
+console.log(details)  
+
+ReactDOM.render(
+  details,
+  document.getElementById("root")
+)
