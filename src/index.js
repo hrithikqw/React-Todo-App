6d3d6c94e7676cc92908b64755a07859e1077c9d
hrithik <hrithikm485@gmail.com>
2023-08-31 19:44:53 +0530

    built custom component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-ReactDOM.render((
-<ul>
-<li><h1>Hello all!!</h1></li>
-<li><h2>This my basic react program</h2></li>
-<li><h3>Alrigth!! Get ready for the upcomming projects</h3></li>
-</ul>
-),document.getElementById("root"))
+ReactDOM.render(
+  <div>
+    <Maincontent />
+      </div>,
+
+document.getElementById("root"))
