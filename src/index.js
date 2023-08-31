8d62b0c1f6cf8e5ca392b870322e22d35503d51c
hrithik <hrithikm485@gmail.com>
2023-08-31 18:40:02 +0530

    Did a basic react with ul and il tag

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-ReactDOM.render(<h1>hello guys!!</h1>,document.getElementById("root"))
+ReactDOM.render((
+<ul>
+<li><h1>Hello all!!</h1></li>
+<li><h2>This my basic react program</h2></li>
+<li><h3>Alrigth!! Get ready for the upcomming projects</h3></li>
+</ul>
+),document.getElementById("root"))
