e75cc4a7c44e19dbd88b3c6750a1edecb9e243d6
hrithik <hrithikm485@gmail.com>
2023-09-02 16:06:18 +0530

    added logo

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-const page=(
-  <nav>
-    <h1>Welcome to Our Website </h1>
-    <ol>
-      <li>Home</li>
-      <li>Products</li>
-      <li>Collab</li>
-      <li>About Us</li>
-    </ol>  
-      
-      </nav>
-)
+function Page() {
+  return (
+      <div>
+          <header>
+              <nav>
+                  <img src="./logo.svg" width="200px" />
+              </nav>
+          </header>
+          <h1>Reasons I'm excited to learn React</h1>
+          <ol>
+              <ul>It's a popular library,I'm more likely to get a job as a developer if I know React</ul>
+          </ol>
+          <footer>
+              <small>© 2021 Ziroll development. All rights reserved.</small>
+          </footer>
+      </div>
+  )
+}
 
-ReactDOM.render(page,document.getElementById("root"))   
+ReactDOM.render(<Page />, document.getElementById("root"))  
