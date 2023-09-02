00fc86f6bde548e57787d4f194df7f7bdb67b0f3
hrithik <hrithikm485@gmail.com>
2023-09-02 16:33:39 +0530

    parent/child components program

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
-function Page() {
+function Header(){
+  return(
+  <header>
+  <nav>
+  <img src="./logo.svg" width="200px" />
+  </nav>
+  </header>
+  )
+}
+function MainContent() {
   return (
-      <div>
-          <header>
-              <nav>
-                  <img src="./logo.svg" width="200px" />
-              </nav>
-          </header>
-          <h1>Reasons I'm excited to learn React</h1>
-          <ol>
-              <ul>It's a popular library,I'm more likely to get a job as a developer if I know React</ul>
-          </ol>
-          <footer>
-              <small>© 2021 Ziroll development. All rights reserved.</small>
-          </footer>
-      </div>
+    <div>
+  <h1>Reasons I'm excited to learn React</h1>
+  <ol>
+  <ul>It's a popular library,I'm more likely to get a job as a developer if I know React</ul>
+  </ol>
+  </div>
+  )
+}
+function Footer(){
+  return(
+  <footer>
+  <small>© 2021 Ziroll development. All rights reserved.</small>
+  </footer>
+  )
+}
+function Page() {
+return (
+<div>
+<Header />
+<MainContent />
+<Footer />
+</div>
   )
 }
 
-ReactDOM.render(<Page />, document.getElementById("root"))  
+ReactDOM.render(<Page />, document.getElementById("root"))
